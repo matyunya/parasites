d9f2d8f499244a8a45d941d99a20beba63de95d2
Maxim Matyunin <matyunya@gmail.com>
2016-07-04 21:50:04 +0300

    remove old comments, naming

--- static/input.js
+++ static/input.js
@@ -4,49 +4,68 @@ var left = keyboard(37),
       down = keyboard(40);
 
 left.press = function() {
-  bunny.vx = -5;
-  bunny.vy = 0;
+  snake.vx = -5;
+  snake.vy = 0;
 };
 
 left.release = function() {
-  //Stop the cat
-  if (!right.isDown && bunny.vy === 0) {
-    bunny.vx = bunny.vx*-0.2;
+  if (!right.isDown && snake.vy === 0) {
+    snake.vx = snake.vx*-0.2;
   }
 };
 
-//Up
 up.press = function() {
-  bunny.vy = -5;
-  bunny.vx = 0;
+  snake.vy = -5;
+  snake.vx = 0;
 };
+
 up.release = function() {
-  if (!down.isDown && bunny.vx === 0) {
-    bunny.vy = bunny.vy*-0.2;
+  if (!down.isDown && snake.vx === 0) {
+    snake.vy = snake.vy*-0.2;
   }
 };
 
-//Right
 right.press = function() {
-  bunny.vx = 5;
-  bunny.vy = 0;
+  snake.vx = 5;
+  snake.vy = 0;
 };
 right.release = function() {
-  if (!left.isDown && bunny.vy === 0) {
-    bunny.vx = bunny.vx*-0.2;
+  if (!left.isDown && snake.vy === 0) {
+    snake.vx = snake.vx*-0.2;
   }
 };
 
-//Down
 down.press = function() {
-  bunny.vy = 5;
-  bunny.vx = 0;
+  snake.vy = 5;
+  snake.vx = 0;
 };
+
 down.release = function() {
-  if (!up.isDown && bunny.vx === 0) {
-    bunny.vy = bunny.vy*-0.2;
+  if (!up.isDown && snake.vx === 0) {
+    snake.vy = snake.vy*-0.2;
   }
-  };
+};
+
+function controls(snake) {
+  snake.x += snake.vx;
+  snake.y += snake.vy;
+
+  if (snake.position.x < 0) {
+      snake.position.x = screen.width;
+  }
+
+  if (snake.position.x > screen.width) {
+      snake.position.x = 0;
+  }
+
+  if (snake.position.y < 0) {
+      snake.position.y = screen.height;
+  }
+
+  if (snake.position.y > screen.height) {
+      snake.position.y = 0;
+  }
+}
 
 function keyboard(keyCode) {
   var key = {};
@@ -55,7 +74,6 @@ function keyboard(keyCode) {
   key.isUp = true;
   key.press = undefined;
   key.release = undefined;
-  //The `downHandler`
   key.downHandler = function(event) {
     if (event.keyCode === key.code) {
       if (key.isUp && key.press) key.press();
@@ -65,7 +83,6 @@ function keyboard(keyCode) {
     event.preventDefault();
   };
 
-  //The `upHandler`
   key.upHandler = function(event) {
     if (event.keyCode === key.code) {
       if (key.isDown && key.release) key.release();
@@ -75,7 +92,6 @@ function keyboard(keyCode) {
     event.preventDefault();
   };
 
-  //Attach event listeners
   window.addEventListener(
     "keydown", key.downHandler.bind(key), false
   );
